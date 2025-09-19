3770f3a090381106774b9e64eef236da0c71ca72
mm1ord <1090485174@qq.com>
2025-09-19 22:49:32 +0800

    题目相关接口完成

--- yoj-backend/src/main/java/com/yupi/yoj/controller/QuestionSubmitController.java
+++ yoj-backend/src/main/java/com/yupi/yoj/controller/QuestionSubmitController.java
@@ -1,11 +1,19 @@
 package com.yupi.yoj.controller;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yupi.yoj.annotation.AuthCheck;
 import com.yupi.yoj.common.BaseResponse;
 import com.yupi.yoj.common.ErrorCode;
 import com.yupi.yoj.common.ResultUtils;
+import com.yupi.yoj.constant.UserConstant;
 import com.yupi.yoj.exception.BusinessException;
+import com.yupi.yoj.model.dto.question.QuestionQueryRequest;
 import com.yupi.yoj.model.dto.questionsubmit.QuestionSubmitAddRequest;
+import com.yupi.yoj.model.dto.questionsubmit.QuestionSubmitQueryRequest;
+import com.yupi.yoj.model.entity.Question;
+import com.yupi.yoj.model.entity.QuestionSubmit;
 import com.yupi.yoj.model.entity.User;
+import com.yupi.yoj.model.vo.QuestionSubmitVO;
 import com.yupi.yoj.service.QuestionSubmitService;
 import com.yupi.yoj.service.UserService;
 import lombok.extern.slf4j.Slf4j;
@@ -24,7 +32,7 @@ import javax.servlet.http.HttpServletRequest;
  * @from <a href="https://yupi.icu">编程导航知识星球</a>
  */
 @RestController
-@RequestMapping("/question_submiit")
+@RequestMapping("/question_submit")
 @Slf4j
 public class QuestionSubmitController {
 
@@ -43,7 +51,7 @@ public class QuestionSubmitController {
      */
     @PostMapping("/")
     public BaseResponse<Long> doThumb(@RequestBody QuestionSubmitAddRequest questionSubmitAddRequest,
-            HttpServletRequest request) {
+                                      HttpServletRequest request) {
         if (questionSubmitAddRequest == null || questionSubmitAddRequest.getQuestionId() <= 0) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR);
         }
@@ -52,5 +60,27 @@ public class QuestionSubmitController {
         long questionSubmitId = questionSubmitService.doQuestionSubmit(questionSubmitAddRequest, loginUser);
         return ResultUtils.success(questionSubmitId);
     }
+
+
+    /**
+     * 分页获取题目提交列表（除管理员外，普通用户只能看到非答案等公开信息）
+     *
+     * @param questionSubmitQueryRequest
+     * @param request
+     * @return
+     */
+    @PostMapping("/list/page")
+    public BaseResponse<Page<QuestionSubmitVO>> listQuestionSubmitByPage(@RequestBody QuestionSubmitQueryRequest questionSubmitQueryRequest,
+                                                                         HttpServletRequest request) {
+        long current = questionSubmitQueryRequest.getCurrent();
+        long size = questionSubmitQueryRequest.getPageSize();
+        final User loginUser = userService.getLoginUser(request);
+        //从数据库查询原始的题目提交分页查询
+        Page<QuestionSubmit> questionSubmitPage = questionSubmitService.page(new Page<>(current, size),
+                questionSubmitService.getQueryWrapper( questionSubmitQueryRequest));
+        //返回脱敏信息
+        return ResultUtils.success(questionSubmitService.getQuestionSubmitVOPage(questionSubmitPage, loginUser));
+    }
+
 
 }
--- yoj-backend/src/main/java/com/yupi/yoj/service/impl/QuestionSubmitServiceImpl.java
+++ yoj-backend/src/main/java/com/yupi/yoj/service/impl/QuestionSubmitServiceImpl.java
@@ -1,22 +1,39 @@
 package com.yupi.yoj.service.impl;
 
+import cn.hutool.core.collection.CollUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.yupi.yoj.common.ErrorCode;
+import com.yupi.yoj.constant.CommonConstant;
 import com.yupi.yoj.exception.BusinessException;
+import com.yupi.yoj.model.dto.question.QuestionQueryRequest;
 import com.yupi.yoj.model.dto.questionsubmit.QuestionSubmitAddRequest;
+import com.yupi.yoj.model.dto.questionsubmit.QuestionSubmitQueryRequest;
 import com.yupi.yoj.model.entity.Question;
 import com.yupi.yoj.model.entity.QuestionSubmit;
 import com.yupi.yoj.model.entity.User;
 import com.yupi.yoj.model.enums.QuestionSubmitLanguageEnum;
 import com.yupi.yoj.model.enums.QuestionSubmitStatusEnum;
+import com.yupi.yoj.model.vo.QuestionSubmitVO;
+import com.yupi.yoj.model.vo.QuestionVO;
+import com.yupi.yoj.model.vo.UserVO;
 import com.yupi.yoj.service.QuestionService;
 import com.yupi.yoj.service.QuestionSubmitService;
 import com.yupi.yoj.mapper.QuestionSubmitMapper;
+import com.yupi.yoj.service.UserService;
+import com.yupi.yoj.utils.SqlUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author 10904
@@ -28,6 +45,9 @@ public class QuestionSubmitServiceImpl extends ServiceImpl<QuestionSubmitMapper,
         implements QuestionSubmitService {
     @Resource
     private QuestionService questionService;
+
+    @Resource
+    private UserService userService;
 
     /**
      * 提交题目
@@ -66,6 +86,61 @@ public class QuestionSubmitServiceImpl extends ServiceImpl<QuestionSubmitMapper,
             throw new BusinessException(ErrorCode.SYSTEM_ERROR, "数据提交失败");
         }
         return questionSubmit.getId();
+    }
+
+    /**
+     * 获取查询包装类
+     *
+     * @param questionSubmitQueryRequest
+     * @return
+     */
+    @Override
+    public QueryWrapper<QuestionSubmit> getQueryWrapper(QuestionSubmitQueryRequest questionSubmitQueryRequest) {
+        QueryWrapper<QuestionSubmit> queryWrapper = new QueryWrapper<>();
+        if (questionSubmitQueryRequest == null) {
+            return queryWrapper;
+        }
+
+        String language = questionSubmitQueryRequest.getLanguage();
+        Integer status = questionSubmitQueryRequest.getStatus();
+        Long questionId = questionSubmitQueryRequest.getQuestionId();
+        Long userId = questionSubmitQueryRequest.getUserId();
+        String sortField = questionSubmitQueryRequest.getSortField();
+        String sortOrder = questionSubmitQueryRequest.getSortOrder();
+
+        queryWrapper.eq(StringUtils.isNotBlank(language), "language", language);
+        queryWrapper.eq(ObjectUtils.isNotEmpty(userId), "userId", userId);
+        queryWrapper.eq(ObjectUtils.isNotEmpty(questionId), "questionId", questionId);
+        queryWrapper.eq(QuestionSubmitStatusEnum.getEnumByValue(status) != null, "status", status);
+        queryWrapper.orderBy(SqlUtils.validSortField(sortField), sortOrder.equals(CommonConstant.SORT_ORDER_ASC),
+                sortField);
+        return queryWrapper;
+    }
+
+
+    @Override
+    public QuestionSubmitVO getQuestionSubmitVO(QuestionSubmit questionSubmit, User loginUser) {
+        QuestionSubmitVO questionSubmitVO = QuestionSubmitVO.objToVo(questionSubmit);
+        // 脱敏
+        long userId = loginUser.getId();
+        if (userId != questionSubmit.getUserId() && !userService.isAdmin(loginUser) ) {
+            questionSubmitVO.setCode(null);
+        }
+        return questionSubmitVO;
+    }
+
+    @Override
+    public Page<QuestionSubmitVO> getQuestionSubmitVOPage(Page<QuestionSubmit> questionSubmitPage, User loginUser) {
+        List<QuestionSubmit> questionSubmitList = questionSubmitPage.getRecords();
+        Page<QuestionSubmitVO> questionSubmitVOPage = new Page<>(questionSubmitPage.getCurrent(), questionSubmitPage.getSize(), questionSubmitPage.getTotal());
+        if (CollUtil.isEmpty(questionSubmitList)) {
+            return questionSubmitVOPage;
+        }
+        List<QuestionSubmitVO> questionSubmitVOList = questionSubmitList.stream()
+                .map(questionSubmit -> getQuestionSubmitVO(questionSubmit, loginUser))
+                .collect(Collectors.toList());
+        questionSubmitVOPage.setRecords(questionSubmitVOList);
+        return questionSubmitVOPage;
     }
 
 }
